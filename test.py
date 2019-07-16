9cf382a98739f2c27ba0c9d06b465f212f8d1af2
Karolis Ramanauskas <karolisr@gmail.com>
2019-07-16 06:16:40 -0500

    Fixing test file.

--- test.py
+++ test.py
@@ -3,5 +3,5 @@
 """Rudimentary Testing"""
 
 from ncbi_taxonomy_local import Taxonomy
-Taxonomy.init(data_dir_path='~/NCBI_TAXONOMY')
+Taxonomy.init(data_dir_path='./NCBI_TAXONOMY')
 Taxonomy.update()
