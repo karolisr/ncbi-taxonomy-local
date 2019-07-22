04b7f9ebcd222b3f3182d06f18ef9377897f87fc
Karolis Ramanauskas <karolisr@gmail.com>
2019-07-21 21:12:14 -0500

    Change license. Trying setyp.py script.

--- ncbi_taxonomy_local/__init__.py
+++ ncbi_taxonomy_local/__init__.py
@@ -2,10 +2,11 @@
 
 from ncbi_taxonomy_local.taxonomy import Taxonomy, taxonomy
 
-__version__ = '0.0.0'
+__version__ = '0.0.1'
 __author__ = 'Karolis Ramanauskas'
 __author_email__ = 'kraman2@uic.edu'
 __copyright__ = 'Copyright 2019 Karolis Ramanauskas'
-__license__ = 'MIT License'
+__license__ = ('Creative Commons Attribution-ShareAlike 4.0 International'
+               'License')
 
 __all__ = ['Taxonomy', 'taxonomy']
